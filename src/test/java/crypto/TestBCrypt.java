8d0f46a024c284b176b94ef9f93c901856b970b9
Titi Wangsa bin Damhore <titi.wangsa.damhore@tabuk-tech.com>
2013-08-10 19:56:14 +0800

    some hashed values

--- src/test/java/crypto/TestBCrypt.java
+++ src/test/java/crypto/TestBCrypt.java
@@ -9,6 +9,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class TestBCrypt {
 
 	@Test
+	public void testEncodingVettel87a() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		assertTrue(encoder.matches("vettel87",
+				"$2a$10$a3WSjOe3BwdL/ou6STvPcuI6ikpm6vOVnz8qIeGAdfwGaZBEPoIFW"));
+	}
+
+	@Test
+	public void testEncodingVettel87b() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		assertTrue(encoder.matches("vettel87",
+				"$2a$10$SJF.osDLAcg6WmToYT7TW.6iJe7wh.JghWUFt91xHnQSCPARM3F7u"));
+	}
+
+	@Test
+	public void testEncodingAdmin() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		assertTrue(encoder.matches("admin",
+				"$2a$10$3Frma5rvcwKUi9EkIxP6nejNWh9Ex9nak6KljbR/khHTcFQjUQF26"));
+	}
+
+	@Test
 	public void testEncoding() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		assertTrue(encoder.matches("hello",
@@ -21,16 +42,19 @@ public class TestBCrypt {
 		String encoded = encoder.encode("a");
 		assertEquals(60, encoded.length());
 	}
+
 	@Test
 	public void testLengthMiddle() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String encoded = encoder.encode("hello");
 		assertEquals(60, encoded.length());
 	}
+
 	@Test
 	public void testLengthLong() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		String encoded = encoder.encode("======this should be longer than sixty characters wide.======");
+		String encoded = encoder
+				.encode("======this should be longer than sixty characters wide.======");
 		assertEquals(60, encoded.length());
 	}
 
